b477793492c8755750b1d5fdcb0c1812e06f304c
Wancharle Sebastiao Quirino <wancharle@gmail.com>
2016-04-28 20:29:33 +0000

    correcao emoji e certs android

--- assinar.sh
+++ assinar.sh
@@ -1,5 +1,5 @@
-rm my-release-key.keystore
+#rm my-release-key.keystore
 cordova build --release android
-keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+#keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
 zipalign -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk android-release.apk
